2c63672d670e498948f4d6234c1c8bf97b2baf8a
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2018-12-29 13:31:35 -0600

    added login controll

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :phone, :admin, :creator])
   end
+
+  private
+
+  def require_creator
+    unless user_signed_in? && (current_user.admin? || current_user.creator?)
+      flash[:alert] = "You must be logged in to access this section"
+      redirect_to new_user_session_path # halts request cycle
+    end
+  end
+
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_creator, only: [:new, :edit, :update, :create, :destroy]
 
   # GET /articles
   # GET /articles.json
