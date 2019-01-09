9075557acf3a312a5bfda4c6d831b80937e1f6c5
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2019-01-08 20:55:49 -0600

    added homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
+    @other = []
+    @articles = Article.order(created_at: :desc).last(10)
+    @other.push(Article.find_by(category_id: Category.find_by(name: 'International').id))
+    @other.push(Article.find_by(category_id: Category.find_by(name: 'US News').id))
+    @other.push(Article.find_by(category_id: Category.find_by(name: 'Society').id))
+    @other.push(Article.find_by(category_id: Category.find_by(name: 'Culture').id))
+    @other.push(Article.find_by(category_id: Category.find_by(name: 'Science & Technology').id))
+    @other.push(Article.find_by(category_id: Category.find_by(name: 'Sports').id))
+    @other.push(Article.find_by(category_id: Category.find_by(name: 'People').id))
+    @other.push(Article.find_by(category_id: Category.find_by(name: 'Media').id))
   end
 
   def about
