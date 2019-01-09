52e622a8578f21c68918aedffdc3718f18f0272f
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2019-01-08 20:54:01 -0600

    Added categories in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 admin = User.create(first_name: 'Admin', last_name: 'Admin', username: 'admin', phone: ENV["ADMIN_PHONE"], admin: true, creator: true, email: ENV["ADMIN_EMAIL"], password: ENV["ADMIN_PASSWORD"], password_confirmation: ENV["ADMIN_PASSWORD"])
 admin.save
+categories = Category.create([{name: 'International'}, {name: 'US News'}, {name: 'Society'}, {name: 'Culture'}, {name: 'Science & Technology'}, {name: 'Sports'}, {name: 'People'}, {name: 'Media'}])
+categories.each do |category|
+  category.save
+end
